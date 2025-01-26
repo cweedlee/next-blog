2d83280c49d004ff2f18356f1fc5b8e2a0e2abd9
Kimmy Haecho <118002501+siwolee@users.noreply.github.com>
2025-01-26 17:14:48 +0900

    chore: pre codemode

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,11 +1,7 @@
-// "use client";
-
 import Post from "@/components/post";
 
 export default async function Page({ params }: { params: { postId: string } }) {
-  console.log("Page", process.env.NEXT_PUBLIC_NOTION_DATABASE_ID);
-  const postId = await params.postId;
-  console.log(postId);
+  const { postId } = await params;
 
   return <Post id={postId} />;
 }
